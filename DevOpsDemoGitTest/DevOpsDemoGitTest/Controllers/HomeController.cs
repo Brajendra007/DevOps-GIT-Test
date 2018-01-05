d18bdd03fd807f4bf69bbd084266636d92e55933
brajendra <brajendra@test.com>
2018-01-05 22:09:30 +0530

    Hpme Controller Page

--- DevOpsDemoGitTest/DevOpsDemoGitTest/Controllers/HomeController.cs
+++ DevOpsDemoGitTest/DevOpsDemoGitTest/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace DevOpsDemoGitTest.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "This is your descroption page.";
 
             return View();
         }
